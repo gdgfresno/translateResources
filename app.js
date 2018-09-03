6abb6530d35704a5062b69a69adaf5dfa28aaa9c
MrCsabaToth <csaba.toth.us@gmail.com>
2018-09-03 00:23:38 -0700

    Trying to implement 2 level JSON for DeLorean. Unfortunately a rate limiter kicked in on Google's end.

--- app.js
+++ app.js
@@ -20,50 +20,73 @@ var app = express();
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-app.get('/translate', function(req, res) {
-  var fs = require('fs');
-  var contentText = fs.readFileSync('resources.json', 'utf8');
+var options = {};
+var sourceLang = 'en';
+var targetLang = '';
+var urlPrefix = ''
+var translated = {};
 
-  var sourceLang = 'en';
-  var targetLang = req.query.lang;  // 'es', 'hmn'
-  var urlPrefix = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" 
-      + sourceLang + "&tl=" + targetLang + "&dt=t&q=";
+var delay = function(milliseconds) {
+  if (!milliseconds)
+    milliseconds = Math.floor(Math.random() * 2000);
+  setTimeout(function() {}, milliseconds);
+}
 
-  var translated = {};
-  var contentJson = JSON.parse(contentText);
-
-  async.eachSeries(Object.keys(contentJson),
-      function (key, done) {  // iterator
-        var options = {};
-        request.get(urlPrefix + encodeURI(contentJson[key]), options, function(error, response, body) { 
+var translatePart = function(contentPart, keyOuter) {
+  async.eachSeries(Object.keys(contentPart),
+    function (keyInner, doneInner) {  // iterator
+      delay();
+      request.get(urlPrefix + encodeURI(contentPart[keyInner]), options, function(error, response, body) { 
           if (error) {
             res.send(error);
             return;
           };
-          var responseJson = JSON.parse(response.body.replace(/\,{2,}/gi, ','));
-          var translation = '';
+          console.log(response.body);
+          let responseJson = JSON.parse(response.body.replace(/\,{2,}/gi, ','));
+          let translation = '';
           responseJson[0].forEach(sentence_tuple => {
             translation += sentence_tuple[0];
           });
-          translated[key] = translation;
-          done(null);
+          translated[keyOuter][keyInner] = translation;
+          delay();
+          doneInner(null);
         });
-      },
-      function (err) {
-        // global callback for async.eachSeries
-        if (err) {
-          res.send(err);
-        } else {
-          var targetFileName = 'resources.' + targetLang + '.json';
-          fs.writeFile(targetFileName, JSON.stringify(translated, null, 2), function (err) {
-            if (err) {
-              res.send(err);
-            } else {
-              res.send(targetLang + ' translation: ' + targetFileName);
-            }
-          });
-        }
-      });
+    },
+    function (err) {
+      // global callback for async.eachSeries
+      if (err) {
+        res.send(err);
+      }
+    });
+}
+
+app.get('/translate', function(req, res) {
+  let fs = require('fs');
+  let sourceFileName = 'locale-' + sourceLang + '.json';
+  let contentText = fs.readFileSync(sourceFileName, 'utf8');
+
+  targetLang = req.query.lang;  // 'es', 'hmn'
+  urlPrefix = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" 
+    + sourceLang + "&tl=" + targetLang + "&dt=t&q=";
+
+  let contentJson = JSON.parse(contentText);
+
+  for (let key in contentJson) {
+    if (contentJson.hasOwnProperty(key)) {
+      translated[key] = {};
+      translatePart(contentJson[key], key);
+    }
+    delay();
+  }
+
+  let targetFileName = 'locale-' + targetLang + '.json';
+  fs.writeFile(targetFileName, JSON.stringify(translated, null, 2), function (err) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(targetLang + ' translation: ' + targetFileName);
+    }
+  });
 });
 
 // Start server on the specified port and binding host
